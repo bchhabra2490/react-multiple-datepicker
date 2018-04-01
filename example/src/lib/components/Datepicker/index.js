2ccba750d14869642fc1a557e5a26946e9c1acda
bchhabra2490 <bchhabra2490@gmail.com>
2018-04-01 15:01:37 +0530

    Prefilled dates

--- example/src/lib/components/Datepicker/index.js
+++ example/src/lib/components/Datepicker/index.js
@@ -75,7 +75,7 @@ class DatePicker extends Component {
     this.state = {
       view: DateUtilities.clone(def),
       selected: DateUtilities.clone(def),
-      selectedDates: props.selected ? [DateUtilities.clone(def)] : [],
+      selectedDates:props.selectedDates?props.selectedDates:(props.selected ? [DateUtilities.clone(def)] : []),
       minDate: null,
       maxDate: null,
       open: false,
